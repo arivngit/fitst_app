350cb29c6d5f9408e5072940eb93801920cd500f
Arivarasan <l.arivarasan@gmail.com>
2011-12-20 13:23:18 -0800

    First git Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyDeployment::Application.config.secret_token = '996dcb3e3cab7a9d66acffd7087cb30168afd485206b6d42cfe6cf6e4c493a2e571c87d786e704c8db3090b2deaff1e3485bdc88fe34c047632abb355126315f'
